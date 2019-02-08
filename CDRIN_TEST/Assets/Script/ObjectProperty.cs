1b56970c2bf628085b85a3f9857e5f4ffa364dd0
Erfas <fortin.eric.24@hotmail.com>
2019-02-08 14:54:29 -0500

    zone autour de l'objet, changement valeur
    zone autour de l'objet modifiable dans l'inspecteur, chagement de variable possible dans l'inspecteur

---
+++ CDRIN_TEST/Assets/Script/ObjectProperty.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectProperty : MonoBehaviour
+{
+
+    public float price;
+    public string objectName;
+    public string description;
+    public float resistance;
+
+    [HideInInspector]
+    public Vector3 rectOffset;
+    [HideInInspector]
+    public Vector3 rectSize;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(this.transform.position + rectOffset, this.transform.localScale + rectSize);
+        Debug.Log("working ?");
+    }
+}
